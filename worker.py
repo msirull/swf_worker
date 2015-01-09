f709221452c819f363ec2ab9a51fa05e91ba7f09
msirull <martin@sirull.com>
2015-01-08 19:50:06 -0800

    A bit of this and that

--- worker.py
+++ worker.py
@@ -1,7 +1,8 @@
 import boto.swf.layer2 as swf
 from boto.dynamodb2.table import Table
-import boto.ec2, redis, boto.cloudformation
+import boto.ec2, redis, boto.cloudformation, logging, time
 from boto.utils import get_instance_metadata
+logging.basicConfig(filename='/var/log/worker/worker.log',level=logging.INFO)
 
 region = get_instance_metadata()['placement']['availability-zone'][:-1]
 ec2_conn = boto.ec2.connect_to_region(region)
@@ -17,7 +18,7 @@ VERSION = '1.0'
 TASKLIST = 'default'
 
 r = redis.StrictRedis(host='tes-pu-3jpt4w8eifgu.qpeias.0001.usw2.cache.amazonaws.com', port = 6379, db=0)
-dynamo_config=cf_conn.describe_stack_resource(tags['aws:cloudformation:stack-name'],'configTable1')
+dynamo_config=cf_conn.describe_stack_resource(tags['aws:cloudformation:stack-name'],'ServiceConfigTable')
 tbl=dynamo_config['DescribeStackResourceResponse']['DescribeStackResourceResult']['StackResourceDetail']['PhysicalResourceId']
 table_obj = Table(tbl)
 
@@ -32,10 +33,11 @@ class HelloWorker(swf.ActivityWorker):
             activity_task = self.poll()
             if 'activityId' in activity_task:
                 requestid = activity_task['workflowExecution']['workflowId']
-                result = table_obj.get_item(env=tags['environment'], option='demo')
+                result = table_obj.get_item(env='demo')
                 data = result['data']
                 r.set(requestid, data)
                 print 'Hello, World!'
+                logging.info(str(time.time()))
                 self.complete()
             if activity_task is False:
                 activity_task = "Nothing to do"
